8aa0f030415d02fe81122afbce043684f2b128a5
Seth West <kowit010@umn.edu>
2014-05-21 10:52:55 -0500

    Modification to runme.sh

--- csci/python/VIRO/runme.sh
+++ csci/python/VIRO/runme.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 
+##############
+## runme.sh
+##############
+
+## File for convenient execution of VIRO simulation.
+
+##############################################################################
+## For educational reference only! See DISCLAIMER on the home page of my repo.
+##############################################################################
+
+##############################################################################
+## Copyright 2014 Seth West
+## Licensed under the Apache License, Version 2.0 (the "License");    
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at:
+## 
+## http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License in distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+## implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##############################################################################
+
 #give user a choice of which tests to run
 
 clear
